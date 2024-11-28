e1381d29eb28a7cefa393b79a1c485613a2c39d9
ErR0rpj <priyanshjain301@gmail.com>
2024-11-28 11:30:03 +0530

    CF partial 988 F

--- codeforces/⭐codeforeces_988_F.go
+++ codeforces/⭐codeforeces_988_F.go
@@ -5,6 +5,10 @@ import (
 	"main/templates"
 )
 
+func ableToKill(position []int, currentIndex int, dividerIndex int, divider []int) bool {
+	return false
+}
+
 // https://codeforces.com/contest/2037/problem/F
 func Codeforces_988_F() {
 	var t int
@@ -37,11 +41,45 @@ func Codeforces_988_F() {
 				maxiAttack = health[i]
 			}
 		}
-
+		// 5 5 3
+		// 7 7 7 7 7
+		// 1 2 3 4 5
 		for miniAttack <= maxiAttack {
+			var intervals [][]int
 			currentAttack = (miniAttack + maxiAttack) / 2
-
+			for i := range health {
+				powerReq := health[1] / currentAttack
+				if health[i]%currentAttack > 0 {
+					powerReq += 1
+				}
+				if powerReq <= m {
+					endInterval := position[i] + (m - powerReq)
+					startInterval := position[i] - (m - powerReq)
+					interval := make([]int, 2)
+					interval[0] = startInterval
+					interval[1] = endInterval
+					intervals = append(intervals, interval)
+				}
+			}
+			possible := false
+			for i := range intervals {
+				if ableToKill() {
+					possible = true
+					break
+				}
+			}
+			if possible {
+				maxiAttack = currentAttack - 1
+				if ans == -1 {
+					ans = currentAttack
+				}
+				if currentAttack < ans {
+					ans = currentAttack
+				}
+			} else {
+				miniAttack = currentAttack + 1
+			}
 		}
-
+		fmt.Println(ans)
 	}
 }
